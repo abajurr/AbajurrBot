14b1afdd500457a234d48aac1e39c30012dad449
Kelvin Wieth <kelvin.wieth@partner.bmw.com.br>
2022-04-18 00:13:54 -0300

    add first unit tests

---
+++ tests/AbajurrBot.Core.Tests/Models/RoleTest.cs
@@ -0,0 +1,30 @@
+﻿using AbajurrBot.Core.Models;
+using Xunit;
+
+namespace AbajurrBot.Core.Tests.Models
+{
+    public class RoleTest
+    {
+        [Fact]
+        public void Constructor_WithoutName_NameIsEmpty()
+        {
+            // Arrange
+            // Act
+            var role = new Role();
+
+            // Assert
+            Assert.Empty(role.Name);
+        }
+
+        [Fact]
+        public void Constructor_WithoutUsers_UsersIsEmpty()
+        {
+            // Arrange
+            // Act
+            var role = new Role();
+
+            // Assert
+            Assert.Empty(role.Users);
+        }
+    }
+}
